6de32a4997ff3202283517acf7cda4abe4f997da
Asad Aijaz Arain <aijaz.asadi@gmail.com>
2024-05-11 22:36:17 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,8 @@ catalog_dataframe = session.sql("SELECT * from ZENAS_ATHLEISURE_DB.products.cata
 selected_item = catalog_dataframe[0]
 
 st.image(selected_item["DIRECT_URL"], use_column_width  = "auto", caption="Our warm, comfortable, " + selected_color + " sweatsuit!")
-st.write("**Price:** " + str(selected_item["PRICE"]))
+st.metric(label="Price", value=str(selected_item["PRICE"]))
+# st.write("**Price:** " + str(selected_item["PRICE"]))
 st.write("**Sizes Available:** " + selected_item["SIZE_LIST"])
 st.write(selected_item["UPSELL_PRODUCT_DESC"])
 
