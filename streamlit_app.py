93650321722bd47120e266e711b9d45add13c117
Asad Aijaz Arain <aijaz.asadi@gmail.com>
2024-05-11 22:14:00 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ st.title('Zena\'s Amazing Athleisure Catalog')
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-# colors_dataframe = session.table("CATALOG_FOR_WEBSITE").select(col('COLOR_OR_STYLE'))   
-colors_dataframe = session.sql("SELECT COLOR_OR_STYLE from ZENAS_ATHLEISURE_DB.products.catalog_for_website").collect()
+colors_dataframe = session.table("ZENAS_ATHLEISURE_DB.products.catalog_for_website").select(col('COLOR_OR_STYLE'))   
+# colors_dataframe = session.sql("SELECT COLOR_OR_STYLE from ZENAS_ATHLEISURE_DB.products.catalog_for_website").collect()
 
 option = st.selectbox(
    "Pick a sweatsuit color or style",
